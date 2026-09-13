6cb96550b9dc9d152963601d61b7ed8fd2a41149
unblockedgames99x-code <unblockedgames99x-code@users.noreply.github.com>
2026-09-12 23:32:49 -0500

    Optimize desktop responsiveness and restore complete assets

--- neo-games/app.js
+++ neo-games/app.js
@@ -444,7 +444,7 @@
 
   function requestProxiedEmbed(frame, target) {
     if (!isEmbedded()) return false;
-    var wrapper = new URL("https://fastly.jsdelivr.net/gh/unblockedgames99x-code/neo-os-browser-cdn@206a9d99284cd9b8da58e14553c0dcdf6d476ac1/NEO-BROWSER/launch.svg", document.baseURI);
+    var wrapper = new URL("https://fastly.jsdelivr.net/gh/unblockedgames99x-code/neo-os-browser-cdn@b4ee294567d1bebde9427266869c00e1f0a9d006/NEO-BROWSER/launch.svg", document.baseURI);
     wrapper.searchParams.set("neo-app-mode", "1");
     wrapper.searchParams.set("neo-custom-app", "1");
     wrapper.searchParams.set("neo-app-target", target);
